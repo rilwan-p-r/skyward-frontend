e198822b4d482a88c4067dc7ecf45bf6e435de13
rilwan-p-r <rilwanpr@gmail.com>
2024-10-04 21:08:15 +0530

    updated

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { refreshTokenAxiosInterceptors } from "./interceptors/refreshTokenInterceptor";
-const BASE_URL = import.meta.env.VITE_BACKEND_PORT;
+const BASE_URL = import.meta.env.VITE_SOCKET_SERVER_URL;
 console.log('BaseUrl:', BASE_URL);
 
 const Api = axios.create({
